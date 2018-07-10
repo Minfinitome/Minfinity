f8cccc36e8ca88d61d7a0a00b6fb087370b9da39
mTinfinity <munmastiv@gmail.com>
2018-07-10 21:35:28 +0700

    edit many thing

--- sketch.js
+++ sketch.js
@@ -5,6 +5,12 @@ var number = 1;
 var add = 235;
 var song;
 var font1;
+var _pos = [100];
+var _incre = [100];
+for(var i = 0; i< 101; i++) {
+  _pos[i] = 0;
+  _incre[i] = 0;
+}
 
 function preload() {
   song = loadSound("https://mtinfinity.github.io/Minfinity/payday.mp3");
@@ -17,7 +23,6 @@ function setup() {
   gravity = createVector(0, 0.15);
   stroke(255);
   strokeWeight(4);
-  background(0);
   song.loop();
 }
 
@@ -45,14 +50,15 @@ function fire() {
 }
 
 function coutUp() {
-  textFont("font1", 20);
+  textFont("font1");
   noStroke();
   fill(random(150,255));
+  textSize(25);
   if(time < 1200) {
-    text("Starting at 27 - 01 - 2016", 100, 100);
-    if(time > 600) text("Today is 07 - 10 - 2018 ...", 100, 130);
+  displayText("Bắt đầu từ 27-01-2016", 100, 100, 0);
+  if(time > 600) text("Hôm nay đã là 07-10-2018 ...", 100, 130);
   }
-  else if(time < 2000) {
+  else if(time < 2600) {
 	text(number, 100, windowHeight/5);
 	if(number > 365) text("một năm dài", windowWidth/5, windowHeight*2/5 - 50);
 	if(number >= 600) text("days", windowWidth*2/5, windowHeight*2/5 - 50);	
@@ -72,4 +78,15 @@ function coutUp() {
   else {number = 618; add = 253;}
   }
 }
+
+function displayText(n, x, y, de) {
+  //textFont("font1"); 
+  noStroke();
+  fill(255);
+  textSize(25);
+  text(n.substring(_pos[de], _pos[de] + 1), x + _incre[de], y);
+  _pos[de]++; _incre[de] += 2;
+  if(_pos > n.length) {_pos = 0; _incre = 0;}
+}
+
 
