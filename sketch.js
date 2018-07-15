e79a9605755d6face97ecd1b5c6185ab067b9444
mTinfinity <munmastiv@gmail.com>
2018-07-15 10:12:22 +0700

    a-11

--- sketch.js
+++ sketch.js
@@ -99,7 +99,7 @@ function textOnScreen() {
   if(time > 900) {
 	  text(number + " ngày", column, 100 + exrow*3);
 	  if(number >= 365) displayText("là một năm ...", column, 100 + exrow*4, 5);
-    if(number >= 600) text("cộng thêm với " + add, column*3 - 24, 100 + exrow*4);
+    if(number >= 600) text("cộng thêm với " + add, column*3 - 28, 100 + exrow*4);
     if(number == 618) displayText("ngày nữa !", column*5 - 12, 100 + exrow*4, 6);
   }
 }
