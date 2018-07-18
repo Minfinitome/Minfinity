5db1829098f464c73fcd49339f8523bc3217c259
mTinfinity <munmastiv@gmail.com>
2018-07-18 10:44:04 +0700

    add image

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var number = 1;
 var add = 237;
 var heart;
 var typing;
+var img = [];
 
 // bien cua preLoad
 var song;
@@ -16,21 +17,19 @@ var _pos = [100];
 for(var i = 0; i< 101; i++) _pos[i] = 1;
 var seconz = 0;
 
-// bien hien thi text
+// bien hien thi
 var column = 100;
 var exrow = 45;
 
 function preload() {
   song = loadSound("https://mtinfinity.github.io/Minfinity/payday.mp3");
-  typing = loadSound("https://mtinfinity.github.io/Minfinity/typing.mp3");
+  img[0] = loadImage("https://mtinfinity.github.io/Minfinity/code.png");
 }
 
 function setup() {
-  createCanvas(1350, 645);
-  colorMode(RGB);
+  createCanvas(1350, 645); colorMode(RGB);
   gravity = createVector(0, 0.15);
-  stroke(255);
-  strokeWeight(4);
+  stroke(255); strokeWeight(4);
   song.loop();
 }
 
@@ -38,9 +37,9 @@ function draw() {
   background(0);
   fire();
   if(seconz > 7) coutUp();
-  textOnScreen();
+  displayOnScreen();
   time += 1;
-  if(time % 250 == 0) seconz += 1;
+  if(time % 200 == 0) seconz += 1;
 }
 
 function fire() {
@@ -76,26 +75,27 @@ function coutUp() {
 }
 
 function displayText(n, x, y, z) {
-  noStroke();
-  fill(255);
+  noStroke(); fill(255);
   textFont("Amatic SC", 40);
   text(n.substring(0, _pos[z]), x, y);
   if(_pos[z] < n.length) _pos[z]++;
   else _pos[z] = n.length; 
 }
 
-function textOnScreen() {
-  textFont("Amatic SC", 40);
-  noStroke();
-  fill(255);
+function displayImg(img, x, y) {
+
+}
+
+function displayOnScreen() {
+  textFont("Amatic SC", 40); noStroke(); fill(255);
   if(seconz < 4) {
     displayText("Hey ...", column, 100, 0);
     if(seconz > 1) displayText("Let's start a journey ...", column, 100 + exrow, 1);
     if(seconz > 2) displayText("It's a short journey, but ... ", column, 100 + exrow*2, 2);
-    if(seconz > 3) displayText("I hope you like it :heart:", column, 100 + exrow*3, 3)
+    if(seconz > 2) displayText("I hope you like it :heart:", column, 100 + exrow*3, 3)
   }
 
-  if(seconz > 4 && seconz < 15) {
+  if(seconz > 4 && seconz < 18) {
     if(seconz > 4) displayText("Bắt đầu từ 27-01-2017", column, 100, 4);
     if(seconz > 5) displayText("Hôm nay đã là 07-10-2018 ...", column, 100 + exrow, 5);
     if(seconz > 6) displayText("Thế là đã qua ...", column, 100 + exrow*2, 6)
@@ -106,6 +106,7 @@ function textOnScreen() {
     }
   }
 
-  //if(seconz > 15 && seconz < 40) {}
+  if(seconz > 18) image(img[0], 20, 20);
+
 }
 
